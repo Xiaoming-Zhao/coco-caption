763f1d5ff83e81abec9e433a5196e0db12dde145
Xiaoming Zhao <xiaoming.zhao9@gmail.com>
2021-01-25 17:56:46 -0600

    [Code] lint

--- pycocoevalcap_self_critical/meteor/meteor.py
+++ pycocoevalcap_self_critical/meteor/meteor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Python wrapper for METEOR implementation, by Xinlei Chen
-# Acknowledge Michael Denkowski for the generous discussion and help 
+# Acknowledge Michael Denkowski for the generous discussion and help
 
 import os
 import sys
@@ -9,22 +9,36 @@ import subprocess
 import threading
 
 # Assumes meteor-1.5.jar is in the same directory as meteor.py.  Change as needed.
-METEOR_JAR = 'meteor-1.5.jar'
+METEOR_JAR = "meteor-1.5.jar"
 # print METEOR_JAR
 
-class Meteor:
 
+class Meteor:
     def __init__(self):
         self.env = os.environ
-        self.env['LC_ALL'] = 'en_US.UTF_8'
-        self.meteor_cmd = ['java', '-jar', '-Xmx2G', METEOR_JAR, \
-                '-', '-', '-stdio', '-l', 'en', '-norm']
-        self.meteor_p = subprocess.Popen(self.meteor_cmd, \
-                cwd=os.path.dirname(os.path.abspath(__file__)), \
-                stdin=subprocess.PIPE, \
-                stdout=subprocess.PIPE, \
-                stderr=subprocess.PIPE,
-                env=self.env, universal_newlines=True, bufsize=1)
+        self.env["LC_ALL"] = "en_US.UTF_8"
+        self.meteor_cmd = [
+            "java",
+            "-jar",
+            "-Xmx2G",
+            METEOR_JAR,
+            "-",
+            "-",
+            "-stdio",
+            "-l",
+            "en",
+            "-norm",
+        ]
+        self.meteor_p = subprocess.Popen(
+            self.meteor_cmd,
+            cwd=os.path.dirname(os.path.abspath(__file__)),
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            env=self.env,
+            universal_newlines=True,
+            bufsize=1,
+        )
         # Used to guarantee thread safety
         self.lock = threading.Lock()
 
@@ -40,16 +54,16 @@ class Meteor:
 
         scores = []
 
-        eval_line = 'EVAL'
+        eval_line = "EVAL"
         self.lock.acquire()
         for res in res_list:
-            tmp_res = res['caption']
-            tmp_gt = gts_dict[res['image_id']]
-            assert(len(tmp_res) == 1)
+            tmp_res = res["caption"]
+            tmp_gt = gts_dict[res["image_id"]]
+            assert len(tmp_res) == 1
             stat = self._stat(tmp_res[0], tmp_gt)
-            eval_line += ' ||| {}'.format(stat)
+            eval_line += " ||| {}".format(stat)
 
-        self.meteor_p.stdin.write('{}\n'.format(eval_line))
+        self.meteor_p.stdin.write("{}\n".format(eval_line))
         # for i in range(0,len(imgIds)):
         for i in range(len(res_list)):
             scores.append(float(self.meteor_p.stdout.readline().strip()))
@@ -63,29 +77,33 @@ class Meteor:
 
     def _stat(self, hypothesis_str, reference_list):
         # SCORE ||| reference 1 words ||| reference n words ||| hypothesis words
-        hypothesis_str = hypothesis_str.replace('|||','').replace('  ',' ')
-        score_line = ' ||| '.join(('SCORE', ' ||| '.join(reference_list), hypothesis_str))
-        self.meteor_p.stdin.write('{}\n'.format(score_line))
+        hypothesis_str = hypothesis_str.replace("|||", "").replace("  ", " ")
+        score_line = " ||| ".join(
+            ("SCORE", " ||| ".join(reference_list), hypothesis_str)
+        )
+        self.meteor_p.stdin.write("{}\n".format(score_line))
         # self.meteor_p.stdin.write(score_line+"\n")
         return self.meteor_p.stdout.readline().strip()
 
     def _score(self, hypothesis_str, reference_list):
         self.lock.acquire()
         # SCORE ||| reference 1 words ||| reference n words ||| hypothesis words
-        hypothesis_str = hypothesis_str.replace('|||','').replace('  ',' ')
-        score_line = ' ||| '.join(('SCORE', ' ||| '.join(reference_list), hypothesis_str))
-        self.meteor_p.stdin.write('{}\n'.format(score_line))
+        hypothesis_str = hypothesis_str.replace("|||", "").replace("  ", " ")
+        score_line = " ||| ".join(
+            ("SCORE", " ||| ".join(reference_list), hypothesis_str)
+        )
+        self.meteor_p.stdin.write("{}\n".format(score_line))
         stats = self.meteor_p.stdout.readline().strip()
-        eval_line = 'EVAL ||| {}'.format(stats)
-        # EVAL ||| stats 
-        self.meteor_p.stdin.write('{}\n'.format(eval_line))
+        eval_line = "EVAL ||| {}".format(stats)
+        # EVAL ||| stats
+        self.meteor_p.stdin.write("{}\n".format(eval_line))
         score = float(self.meteor_p.stdout.readline().strip())
         # bug fix: there are two values returned by the jar file, one average, and one all, so do it twice
         # thanks for Andrej for pointing this out
         score = float(self.meteor_p.stdout.readline().strip())
         self.lock.release()
         return score
- 
+
     def __del__(self):
         self.lock.acquire()
         self.meteor_p.stdin.close()
--- pycocotools/coco.py
+++ pycocotools/coco.py
@@ -1,5 +1,5 @@
-__author__ = 'tylin'
-__version__ = '1.0.1'
+__author__ = "tylin"
+__version__ = "1.0.1"
 # Interface for accessing the Microsoft COCO dataset.
 
 # Microsoft COCO is a large image dataset designed for object detection,
@@ -52,6 +52,7 @@ import numpy as np
 from skimage.draw import polygon
 import copy
 
+
 class COCO:
     def __init__(self, annotation_file=None):
         """
@@ -68,37 +69,37 @@ class COCO:
         self.imgs = []
         self.cats = []
         if not annotation_file == None:
-            print 'loading annotations into memory...'
+            print "loading annotations into memory..."
             time_t = datetime.datetime.utcnow()
-            dataset = json.load(open(annotation_file, 'r'))
+            dataset = json.load(open(annotation_file, "r"))
             print datetime.datetime.utcnow() - time_t
             self.dataset = dataset
             self.createIndex()
 
     def createIndex(self):
         # create index
-        print 'creating index...'
-        imgToAnns = {ann['image_id']: [] for ann in self.dataset['annotations']}
-        anns =      {ann['id']:       [] for ann in self.dataset['annotations']}
-        for ann in self.dataset['annotations']:
-            imgToAnns[ann['image_id']] += [ann]
-            anns[ann['id']] = ann
+        print "creating index..."
+        imgToAnns = {ann["image_id"]: [] for ann in self.dataset["annotations"]}
+        anns = {ann["id"]: [] for ann in self.dataset["annotations"]}
+        for ann in self.dataset["annotations"]:
+            imgToAnns[ann["image_id"]] += [ann]
+            anns[ann["id"]] = ann
 
-        imgs      = {im['id']: {} for im in self.dataset['images']}
-        for img in self.dataset['images']:
-            imgs[img['id']] = img
+        imgs = {im["id"]: {} for im in self.dataset["images"]}
+        for img in self.dataset["images"]:
+            imgs[img["id"]] = img
 
         cats = []
         catToImgs = []
-        if self.dataset['type'] == 'instances':
-            cats = {cat['id']: [] for cat in self.dataset['categories']}
-            for cat in self.dataset['categories']:
-                cats[cat['id']] = cat
-            catToImgs = {cat['id']: [] for cat in self.dataset['categories']}
-            for ann in self.dataset['annotations']:
-                catToImgs[ann['category_id']] += [ann['image_id']]
+        if self.dataset["type"] == "instances":
+            cats = {cat["id"]: [] for cat in self.dataset["categories"]}
+            for cat in self.dataset["categories"]:
+                cats[cat["id"]] = cat
+            catToImgs = {cat["id"]: [] for cat in self.dataset["categories"]}
+            for ann in self.dataset["annotations"]:
+                catToImgs[ann["category_id"]] += [ann["image_id"]]
 
-        print 'index created!'
+        print "index created!"
 
         # create class members
         self.anns = anns
@@ -112,8 +113,8 @@ class COCO:
         Print information about the annotation file.
         :return:
         """
-        for key, value in self.datset['info'].items():
-            print '%s: %s'%(key, value)
+        for key, value in self.datset["info"].items():
+            print "%s: %s" % (key, value)
 
     def getAnnIds(self, imgIds=[], catIds=[], areaRng=[], iscrowd=None):
         """
@@ -128,21 +129,40 @@ class COCO:
         catIds = catIds if type(catIds) == list else [catIds]
 
         if len(imgIds) == len(catIds) == len(areaRng) == 0:
-            anns = self.dataset['annotations']
+            anns = self.dataset["annotations"]
         else:
             if not len(imgIds) == 0:
-                anns = sum([self.imgToAnns[imgId] for imgId in imgIds if imgId in self.imgToAnns],[])
+                anns = sum(
+                    [
+                        self.imgToAnns[imgId]
+                        for imgId in imgIds
+                        if imgId in self.imgToAnns
+                    ],
+                    [],
+                )
             else:
-                anns = self.dataset['annotations']
-            anns = anns if len(catIds)  == 0 else [ann for ann in anns if ann['category_id'] in catIds]
-            anns = anns if len(areaRng) == 0 else [ann for ann in anns if ann['area'] > areaRng[0] and ann['area'] < areaRng[1]]
-        if self.dataset['type'] == 'instances':
+                anns = self.dataset["annotations"]
+            anns = (
+                anns
+                if len(catIds) == 0
+                else [ann for ann in anns if ann["category_id"] in catIds]
+            )
+            anns = (
+                anns
+                if len(areaRng) == 0
+                else [
+                    ann
+                    for ann in anns
+                    if ann["area"] > areaRng[0] and ann["area"] < areaRng[1]
+                ]
+            )
+        if self.dataset["type"] == "instances":
             if not iscrowd == None:
-                ids = [ann['id'] for ann in anns if ann['iscrowd'] == iscrowd]
+                ids = [ann["id"] for ann in anns if ann["iscrowd"] == iscrowd]
             else:
-                ids = [ann['id'] for ann in anns]
+                ids = [ann["id"] for ann in anns]
         else:
-            ids = [ann['id'] for ann in anns]
+            ids = [ann["id"] for ann in anns]
         return ids
 
     def getCatIds(self, catNms=[], supNms=[], catIds=[]):
@@ -158,22 +178,34 @@ class COCO:
         catIds = catIds if type(catIds) == list else [catIds]
 
         if len(catNms) == len(supNms) == len(catIds) == 0:
-            cats = self.dataset['categories']
+            cats = self.dataset["categories"]
         else:
-            cats = self.dataset['categories']
-            cats = cats if len(catNms) == 0 else [cat for cat in cats if cat['name']          in catNms]
-            cats = cats if len(supNms) == 0 else [cat for cat in cats if cat['supercategory'] in supNms]
-            cats = cats if len(catIds) == 0 else [cat for cat in cats if cat['id']            in catIds]
-        ids = [cat['id'] for cat in cats]
+            cats = self.dataset["categories"]
+            cats = (
+                cats
+                if len(catNms) == 0
+                else [cat for cat in cats if cat["name"] in catNms]
+            )
+            cats = (
+                cats
+                if len(supNms) == 0
+                else [cat for cat in cats if cat["supercategory"] in supNms]
+            )
+            cats = (
+                cats
+                if len(catIds) == 0
+                else [cat for cat in cats if cat["id"] in catIds]
+            )
+        ids = [cat["id"] for cat in cats]
         return ids
 
     def getImgIds(self, imgIds=[], catIds=[]):
-        '''
+        """
         Get img ids that satisfy given filter conditions.
         :param imgIds (int array) : get imgs for given ids
         :param catIds (int array) : get imgs with all given cats
         :return: ids (int array)  : integer array of img ids
-        '''
+        """
         imgIds = imgIds if type(imgIds) == list else [imgIds]
         catIds = catIds if type(catIds) == list else [catIds]
 
@@ -229,34 +261,40 @@ class COCO:
         """
         if len(anns) == 0:
             return 0
-        if self.dataset['type'] == 'instances':
+        if self.dataset["type"] == "instances":
             ax = plt.gca()
             polygons = []
             color = []
             for ann in anns:
                 c = np.random.random((1, 3)).tolist()[0]
-                if type(ann['segmentation']) == list:
+                if type(ann["segmentation"]) == list:
                     # polygon
-                    for seg in ann['segmentation']:
-                        poly = np.array(seg).reshape((len(seg)/2, 2))
-                        polygons.append(Polygon(poly, True,alpha=0.4))
+                    for seg in ann["segmentation"]:
+                        poly = np.array(seg).reshape((len(seg) / 2, 2))
+                        polygons.append(Polygon(poly, True, alpha=0.4))
                         color.append(c)
                 else:
                     # mask
-                    mask = COCO.decodeMask(ann['segmentation'])
-                    img = np.ones( (mask.shape[0], mask.shape[1], 3) )
-                    if ann['iscrowd'] == 1:
-                        color_mask = np.array([2.0,166.0,101.0])/255
-                    if ann['iscrowd'] == 0:
+                    mask = COCO.decodeMask(ann["segmentation"])
+                    img = np.ones((mask.shape[0], mask.shape[1], 3))
+                    if ann["iscrowd"] == 1:
+                        color_mask = np.array([2.0, 166.0, 101.0]) / 255
+                    if ann["iscrowd"] == 0:
                         color_mask = np.random.random((1, 3)).tolist()[0]
                     for i in range(3):
-                        img[:,:,i] = color_mask[i]
-                    ax.imshow(np.dstack( (img, mask*0.5) ))
-            p = PatchCollection(polygons, facecolors=color, edgecolors=(0,0,0,1), linewidths=3, alpha=0.4)
+                        img[:, :, i] = color_mask[i]
+                    ax.imshow(np.dstack((img, mask * 0.5)))
+            p = PatchCollection(
+                polygons,
+                facecolors=color,
+                edgecolors=(0, 0, 0, 1),
+                linewidths=3,
+                alpha=0.4,
+            )
             ax.add_collection(p)
-        if self.dataset['type'] == 'captions':
+        if self.dataset["type"] == "captions":
             for ann in anns:
-                print ann['caption']
+                print ann["caption"]
 
     def loadRes(self, resFile):
         """
@@ -265,45 +303,51 @@ class COCO:
         :return: res (obj)         : result api object
         """
         res = COCO()
-        res.dataset['images'] = [img for img in self.dataset['images']]
-        res.dataset['info'] = copy.deepcopy(self.dataset['info'])
-        res.dataset['type'] = copy.deepcopy(self.dataset['type'])
-        res.dataset['licenses'] = copy.deepcopy(self.dataset['licenses'])
+        res.dataset["images"] = [img for img in self.dataset["images"]]
+        res.dataset["info"] = copy.deepcopy(self.dataset["info"])
+        res.dataset["type"] = copy.deepcopy(self.dataset["type"])
+        res.dataset["licenses"] = copy.deepcopy(self.dataset["licenses"])
 
-        print 'Loading and preparing results...     '
+        print "Loading and preparing results...     "
         time_t = datetime.datetime.utcnow()
-        anns    = json.load(open(resFile))
-        assert type(anns) == list, 'results in not an array of objects'
-        annsImgIds = [ann['image_id'] for ann in anns]
-        assert set(annsImgIds) == (set(annsImgIds) & set(self.getImgIds())), \
-               'Results do not correspond to current coco set'
-        if 'caption' in anns[0]:
-            imgIds = set([img['id'] for img in res.dataset['images']]) & set([ann['image_id'] for ann in anns])
-            res.dataset['images'] = [img for img in res.dataset['images'] if img['id'] in imgIds]
+        anns = json.load(open(resFile))
+        assert type(anns) == list, "results in not an array of objects"
+        annsImgIds = [ann["image_id"] for ann in anns]
+        assert set(annsImgIds) == (
+            set(annsImgIds) & set(self.getImgIds())
+        ), "Results do not correspond to current coco set"
+        if "caption" in anns[0]:
+            imgIds = set([img["id"] for img in res.dataset["images"]]) & set(
+                [ann["image_id"] for ann in anns]
+            )
+            res.dataset["images"] = [
+                img for img in res.dataset["images"] if img["id"] in imgIds
+            ]
             for id, ann in enumerate(anns):
-                ann['id'] = id
-        elif 'bbox' in anns[0] and not anns[0]['bbox'] == []:
-            res.dataset['categories'] = copy.deepcopy(self.dataset['categories'])
+                ann["id"] = id
+        elif "bbox" in anns[0] and not anns[0]["bbox"] == []:
+            res.dataset["categories"] = copy.deepcopy(self.dataset["categories"])
             for id, ann in enumerate(anns):
-                bb = ann['bbox']
-                x1, x2, y1, y2 = [bb[0], bb[0]+bb[2], bb[1], bb[1]+bb[3]]
-                ann['segmentation'] = [[x1, y1, x1, y2, x2, y2, x2, y1]]
-                ann['area'] = bb[2]*bb[3]
-                ann['id'] = id
-                ann['iscrowd'] = 0
-        elif 'segmentation' in anns[0]:
-            res.dataset['categories'] = copy.deepcopy(self.dataset['categories'])
+                bb = ann["bbox"]
+                x1, x2, y1, y2 = [bb[0], bb[0] + bb[2], bb[1], bb[1] + bb[3]]
+                ann["segmentation"] = [[x1, y1, x1, y2, x2, y2, x2, y1]]
+                ann["area"] = bb[2] * bb[3]
+                ann["id"] = id
+                ann["iscrowd"] = 0
+        elif "segmentation" in anns[0]:
+            res.dataset["categories"] = copy.deepcopy(self.dataset["categories"])
             for id, ann in enumerate(anns):
-                ann['area']=sum(ann['segmentation']['counts'][2:-1:2])
-                ann['bbox'] = []
-                ann['id'] = id
-                ann['iscrowd'] = 0
-        print 'DONE (t=%0.2fs)'%((datetime.datetime.utcnow() - time_t).total_seconds())
+                ann["area"] = sum(ann["segmentation"]["counts"][2:-1:2])
+                ann["bbox"] = []
+                ann["id"] = id
+                ann["iscrowd"] = 0
+        print "DONE (t=%0.2fs)" % (
+            (datetime.datetime.utcnow() - time_t).total_seconds()
+        )
 
-        res.dataset['annotations'] = anns
+        res.dataset["annotations"] = anns
         res.createIndex()
         return res
-
 
     @staticmethod
     def decodeMask(R):
@@ -312,17 +356,17 @@ class COCO:
         :param   R (object RLE)    : run-length encoding of binary mask
         :return: M (bool 2D array) : decoded binary mask
         """
-        N = len(R['counts'])
-        M = np.zeros( (R['size'][0]*R['size'][1], ))
+        N = len(R["counts"])
+        M = np.zeros((R["size"][0] * R["size"][1],))
         n = 0
         val = 1
         for pos in range(N):
             val = not val
-            for c in range(R['counts'][pos]):
-                R['counts'][pos]
+            for c in range(R["counts"][pos]):
+                R["counts"][pos]
                 M[n] = val
                 n += 1
-        return M.reshape((R['size']), order='F')
+        return M.reshape((R["size"]), order="F")
 
     @staticmethod
     def encodeMask(M):
@@ -332,38 +376,39 @@ class COCO:
         :return: R (object RLE)     : run-length encoding of binary mask
         """
         [h, w] = M.shape
-        M = M.flatten(order='F')
+        M = M.flatten(order="F")
         N = len(M)
         counts_list = []
         pos = 0
         # counts
         counts_list.append(1)
-        diffs = np.logical_xor(M[0:N-1], M[1:N])
+        diffs = np.logical_xor(M[0 : N - 1], M[1:N])
         for diff in diffs:
             if diff:
-                pos +=1
+                pos += 1
                 counts_list.append(1)
             else:
                 counts_list[pos] += 1
         # if array starts from 1. start with 0 counts for 0
         if M[0] == 1:
             counts_list = [0] + counts_list
-        return {'size':      [h, w],
-               'counts':    counts_list ,
-               }
+        return {
+            "size": [h, w],
+            "counts": counts_list,
+        }
 
     @staticmethod
-    def segToMask( S, h, w ):
-         """
+    def segToMask(S, h, w):
+        """
          Convert polygon segmentation to binary mask.
          :param   S (float array)   : polygon segmentation mask
          :param   h (int)           : target mask height
          :param   w (int)           : target mask width
          :return: M (bool 2D array) : binary mask
          """
-         M = np.zeros((h,w), dtype=np.bool)
-         for s in S:
-             N = len(s)
-             rr, cc = polygon(np.array(s[1:N:2]), np.array(s[0:N:2])) # (y, x)
-             M[rr, cc] = 1
-         return M
+        M = np.zeros((h, w), dtype=np.bool)
+        for s in S:
+            N = len(s)
+            rr, cc = polygon(np.array(s[1:N:2]), np.array(s[0:N:2]))  # (y, x)
+            M[rr, cc] = 1
+        return M
